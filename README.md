ab4d29ce32dc91364633c94aba2451d2ac15c230
yoshixmk <pirori67@gmail.com>
2024-04-10 23:35:19 +0900

    set url and run file base argument

--- README.md
+++ README.md
@@ -26,7 +26,9 @@ brew install ffmpeg
 ## How
 
 ```sh
-youtube-dl -f 160 <your-video-url>
-ffmpeg -i "<your>.mp4" -vf fps=1 ./<output-dir>/output%04d.png
-
+youtube-dl --no-part -f 160 -o "./video/%(id)s.%(ext)s" <your-video-url>
+id=$(basename $(ls -t ./video/ | tail -n 1) .mp4)
+mkdir $id
+ffmpeg -i "video/$id.mp4" -vf fps=1 ./$id/output%04d.png
+python3 measure_distance.py $id
 ```
--- measure_distance.py
+++ measure_distance.py
@@ -1,5 +1,11 @@
 import imgsim
 import cv2
+import sys
+import glob
+import re
+
+# get value from command line arg
+video_id = sys.argv[1]
 
 vtr = imgsim.Vectorizer()
 # slide of title and book
@@ -9,14 +15,40 @@ blue_vac = vtr.vectorize(blue)
 pink = cv2.imread("./input/pink.png")
 pink_vac = vtr.vectorize(pink)
 
-for x in range(1475):
-  another_img = cv2.imread("./HyVM0zMvVFc/output%04d.png" %(x + 1))
+# output*.png all files
+files = glob.glob(f'{video_id}/output*.png')
+# Get a number from a file name and get the largest number
+file_numbers = [int(re.search(r'(\d\d\d\d)', file).group()) for file in files]
+max_number = max(file_numbers)
+
+results = []
+for x in range(max_number):
+  another_img = cv2.imread(f"./{video_id}/output%04d.png" %(x + 1))
   another_vac = vtr.vectorize(another_img)
   blue_res = imgsim.distance(blue_vac, another_vac)
   if blue_res < 17:
-    print(x + 1, "sec =", round(blue_res, 2))
+    print(x, "sec =", round(blue_res, 2))
+    results.append(x)
     continue
   pink_res = imgsim.distance(pink_vac, another_vac)
   if pink_res < 16:
-    print(x + 1, "sec =", round(pink_res, 2))
+    print(x, "sec =", round(pink_res, 2))
+    results.append(x)
     continue
+
+# In the case of continuous value of 1.
+# [1, 2, 3, 5, 6, 7, 8, 10] -> [1, 5, 10]
+temp = [results[0]]
+previous = results[0]
+for num in results[1:]:
+  if previous != num - 1:
+    temp.append(num)
+  previous = num
+results = temp
+
+# sec to min and sec
+results = [(x // 60, x % 60) for x in results]
+
+# resultsの分秒を表示
+for min, sec in results:
+  print(f"{min}分 {sec}秒")
